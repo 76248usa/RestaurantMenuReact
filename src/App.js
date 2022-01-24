94766d79eb3b8d86fad3e795fe7c943ed26de79f
Elsabe <76248usa@gmail.com>
2022-01-23 19:03:18 -0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 
 class App extends Component {
 
 render(){
   return (
     <div>
+    <BrowserRouter >
       
-      <Main />
+       <Main />
       
-    </div>   
+    </BrowserRouter> 
+    </div>  
   );
 }
 }
+
 
 export default App;
